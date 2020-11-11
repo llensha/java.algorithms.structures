a28ed1db2910694309cbbf9314875bc6e57ac499
llensha <llensha@mail.ru>
2020-11-11 05:34:05 +0300

    Lesson 4

---
+++ src/lesson4/LinkedQueueImpl.java
@@ -0,0 +1,38 @@
+package lesson4;
+
+import lesson3.myqueue.Queue;
+
+public class LinkedQueueImpl<E> implements Queue<E> {
+
+    private TwoSideLinkedList<E> data;
+
+    public LinkedQueueImpl() {
+        this.data = new TwoSideLinkedListImpl<>();
+    }
+
+    @Override
+    public boolean insert(E value) {
+        data.insertLast(value);
+        return true;
+    }
+
+    @Override
+    public E remove() {
+        return data.removeFirst();
+    }
+
+    @Override
+    public E peekHead() {
+        return data.getFirst();
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+}
